6c4b7ba79ab411054f56764dc7ea2b65234dfe28
Arthur Li <qarthur@uwaterloo.ca>
2024-04-09 10:05:27 -0400

    arthur, minor changes.

--- src/Pages/Home/Music.jsx
+++ src/Pages/Home/Music.jsx
@@ -8,6 +8,15 @@ export default function Music() {
       <p>music</p>
       <section className="musicplayer--section">
         <img src="./img/placeholder1.png" alt="musiclist" />
+        <ul className="list-group">
+          <li className="list-group-item disabled" aria-disabled="true">
+            A disabled item
+          </li>
+          <li className="list-group-item">A second item</li>
+          <li className="list-group-item">A third item</li>
+          <li className="list-group-item">A fourth item</li>
+          <li className="list-group-item">And a fifth one</li>
+        </ul>
       </section>
     </>
   );
